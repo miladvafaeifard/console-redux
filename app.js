a0d2a0cfc2c561e4d098e501c6ae9024146027a4
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2020-03-19 21:57:46 +0100

    Fix code style

--- app.js
+++ app.js
@@ -1,70 +1,70 @@
 const {createStore, applyMiddleware} = require('redux');
 
 const defaultState = {
-    courses: [
-        {
-            name: 'Learning React',
-            topic: 'React',
-        },
-        {
-            name: 'Learning Angular',
-            topic: 'Angular',
-        },
-        {
-            name: 'Using Redux with Angular',
-            topic: 'Angular',
-        }
-    ]
+	courses: [
+		{
+			name: 'Learning React',
+			topic: 'React',
+		},
+		{
+			name: 'Learning Angular',
+			topic: 'Angular',
+		},
+		{
+			name: 'Using Redux with Angular',
+			topic: 'Angular',
+		}
+	]
 };
 
 const logger = store => next => action => {
-    console.log('dispatch ' , action);
-    let result = next(action);
-    console.log('state after action ', store.getState());
-    return result;
+	console.log('dispatch ', action);
+	let result = next(action);
+	console.log('state after action ', store.getState());
+	return result;
 };
 
 const logger2 = store => next => action => {
-  console.log('DEBUG: ', action);
-  return next(action);
+	console.log('DEBUG: ', action);
+	return next(action);
 };
 
 const loggers = [logger, logger2];
 const ADD_COURSE = 'ADD_COURSE';
 
 function reducer(state, action) {
-    switch(action.type) {
-			case ADD_COURSE:
-				return {
-					...state,
-					courses: [...state.courses, action.course]
-				};
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case ADD_COURSE:
+			return {
+				...state,
+				courses: [...state.courses, action.course]
+			};
+		default:
+			return state;
+	}
 }
 
 const store = createStore(reducer, defaultState, applyMiddleware(...loggers));
 
 function addView(viewFunc) {
-    viewFunc(defaultState);
-    store.subscribe(() => {
-        viewFunc(store.getState());
-    });
+	viewFunc(defaultState);
+	store.subscribe(() => {
+		viewFunc(store.getState());
+	});
 }
 
 addView((state) => {
-    console.log(`There are ${state.courses.length} courses in the library`);
+	console.log(`There are ${state.courses.length} courses in the library`);
 });
 
 addView((state) => {
-    console.log(`The latest course in the library: ${state.courses[state.courses.length - 1].name}`);
+	console.log(`The latest course in the library: ${state.courses[state.courses.length - 1].name}`);
 });
 
 store.dispatch({
-    type: ADD_COURSE,
-    course: {
-        name: 'This is the new course',
-        topic: 'Really does not matter'
-    }
+	type: ADD_COURSE,
+	course: {
+		name: 'This is the new course',
+		topic: 'Really does not matter'
+	}
 });
